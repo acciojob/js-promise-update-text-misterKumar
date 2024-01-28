4c82184b759d2112aeb08febabc29d6d7fe129af
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-28 14:45:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const outpu = document.getElementById("output");
+
+function pro() {
+  return new Promise((res, rej) => {
+    if (true) {
+      setTimeout(() => {
+        res("Hello, world!");
+      }, 1000);
+    }
+  });
+}
+
+let hey;
+
+const her = pro()
+  .then((data) => {
+	  outpu.innerText = data;
+  })
+  .catch((err) => {
+    console.log(err);
+  });
